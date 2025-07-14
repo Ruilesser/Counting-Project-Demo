38bdb21c01ead743163bc1c6bbd51e0db7dc9e7f
Linus Yang <linus.yang@ucalgary.ca>
2025-07-14 11:56:20 -0600

    Added addition logic

--- Counting Project Example/Form1.Designer.cs
+++ Counting Project Example/Form1.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textEntry1 = new TextBox();
+            resultBox = new TextBox();
+            textEntry2 = new TextBox();
+            label3 = new Label();
+            addButton = new Button();
+            subButton = new Button();
+            multButton = new Button();
+            divButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(31, 74);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 38);
+            label1.TabIndex = 0;
+            label1.Text = "ENTRY:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(392, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(122, 38);
+            label2.TabIndex = 1;
+            label2.Text = "RESULT:";
+            // 
+            // textEntry1
+            // 
+            textEntry1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textEntry1.Location = new Point(150, 74);
+            textEntry1.Name = "textEntry1";
+            textEntry1.Size = new Size(194, 43);
+            textEntry1.TabIndex = 2;
+            textEntry1.TextChanged += textBox1_TextChanged;
+            // 
+            // resultBox
+            // 
+            resultBox.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            resultBox.Location = new Point(520, 74);
+            resultBox.Name = "resultBox";
+            resultBox.ReadOnly = true;
+            resultBox.Size = new Size(236, 43);
+            resultBox.TabIndex = 3;
+            resultBox.TextChanged += textBox2_TextChanged;
+            // 
+            // textEntry2
+            // 
+            textEntry2.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textEntry2.Location = new Point(174, 161);
+            textEntry2.Name = "textEntry2";
+            textEntry2.Size = new Size(170, 43);
+            textEntry2.TabIndex = 4;
+            textEntry2.TextChanged += textBox3_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(31, 164);
+            label3.Name = "label3";
+            label3.Size = new Size(137, 38);
+            label3.TabIndex = 5;
+            label3.Text = "ENTRY 2:";
+            label3.Click += label3_Click;
+            // 
+            // addButton
+            // 
+            addButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            addButton.Location = new Point(31, 245);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(60, 60);
+            addButton.TabIndex = 6;
+            addButton.Text = "+";
+            addButton.UseVisualStyleBackColor = true;
+            addButton.Click += button1_Click;
+            // 
+            // subButton
+            // 
+            subButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            subButton.Location = new Point(97, 245);
+            subButton.Name = "subButton";
+            subButton.Size = new Size(60, 60);
+            subButton.TabIndex = 7;
+            subButton.Text = "-";
+            subButton.UseVisualStyleBackColor = true;
+            subButton.Click += button2_Click;
+            // 
+            // multButton
+            // 
+            multButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            multButton.Location = new Point(163, 245);
+            multButton.Name = "multButton";
+            multButton.Size = new Size(60, 60);
+            multButton.TabIndex = 8;
+            multButton.Text = "*";
+            multButton.UseVisualStyleBackColor = true;
+            multButton.Click += button3_Click;
+            // 
+            // divButton
+            // 
+            divButton.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            divButton.Location = new Point(229, 245);
+            divButton.Name = "divButton";
+            divButton.Size = new Size(60, 60);
+            divButton.TabIndex = 9;
+            divButton.Text = "/";
+            divButton.UseVisualStyleBackColor = true;
+            divButton.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(divButton);
+            Controls.Add(multButton);
+            Controls.Add(subButton);
+            Controls.Add(addButton);
+            Controls.Add(label3);
+            Controls.Add(textEntry2);
+            Controls.Add(resultBox);
+            Controls.Add(textEntry1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textEntry1;
+        private TextBox resultBox;
+        private TextBox textEntry2;
+        private Label label3;
+        private Button addButton;
+        private Button subButton;
+        private Button multButton;
+        private Button divButton;
     }
 }
--- Counting Project Example/Form1.cs
+++ Counting Project Example/Form1.cs
@@ -2,9 +2,74 @@ namespace Counting_Project_Example
 {
     public partial class Form1 : Form
     {
+
+        private int term1 = 0;
+        private int term2 = 0;
+
+        private int result = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // +
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textEntry1.Text) && !string.IsNullOrEmpty(textEntry2.Text))
+            {
+                term1 = Convert.ToInt32(textEntry1.Text);
+                term2 = Convert.ToInt32(textEntry2.Text);
+                result = term1 + term2;
+                resultBox.Text = result.ToString();
+            }
+        }
+
+
+        // -
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // *
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // /
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Entry 1
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Entry 2
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Result
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
